d40ed71fb4c681e0914cd0f0824841d4cdfd861a
aude <aude.wiki@gmail.com>
2015-08-19 14:45:15 +0200

    Introduce Bot class

--- bootstrap.php
+++ bootstrap.php
@@ -2,20 +2,4 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new Silex\Application();
-
-$app->register( new DerAlex\Silex\YamlConfigServiceProvider( __DIR__ . '/config/config.yml' ) );
-
-$app['app-config'] = $app->share( function() use( $app ) {
-	return new Wikibot\Config( $app['config'] );
-} );
-
-$app->register( new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/templates',
-) );
-
-$app->register( new Silex\Provider\MonologServiceProvider(), array(
-	'monolog.logfile' => __DIR__ . '/log/debug.log'
-) );
-
 //$app->run();
---
+++ src/Bot.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Wikibot;
+
+use DerAlex\Silex\YamlConfigServiceProvider;
+use Knp\Command\Command;
+use Knp\Provider\ConsoleServiceProvider;
+use Silex\Application;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Wikibot\Config;
+use Wikibot\Console\CommandRegistry;
+
+class Bot {
+
+	private $app;
+
+	private $console;
+
+	private $commandRegistry;
+
+	public function __construct() {
+		$this->app = new Application();
+		$this->commandRegistry = new CommandRegistry();
+
+		$this->init();
+	}
+
+	public function init() {
+		$this->app->register( new YamlConfigServiceProvider( __DIR__ . '/../config/config.yml' ) );
+
+		$this->app['app-config'] = $this->app->share( function() {
+			return new Config( $this->app['config'] );
+		} );
+
+		$this->app->register( new TwigServiceProvider(), array(
+			'twig.path' => __DIR__ . '/../templates',
+		) );
+
+		$this->app->register( new MonologServiceProvider(), array(
+			'monolog.logfile' => __DIR__ . '/../log/debug.log'
+		) );
+
+		$this->app->register( new ConsoleServiceProvider(), array(
+			'console.name' => 'Wikibot',
+			'console.version' => '1.0.0',
+			'console.project_directory' => __DIR__ . '/Console'
+		) );
+
+		$this->console = $this->app['console'];
+		$this->initCommands();
+	}
+
+	private function initCommands() {
+		foreach( $this->commandRegistry->getCommands() as $command ) {
+			$this->console->add( $command );
+		}
+	}
+
+	public function registerCommand( Command $command ) {
+		$this->console->add( $command );
+	}
+
+	public function run() {
+		$this->console->run();
+	}
+
+}
